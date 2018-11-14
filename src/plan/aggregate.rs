e29092256e608857e8f9d75d9d4c00cb9501570d
David Bach <David.bach21@gmail.com>
2018-11-14 14:31:23 +0100

    Multiple Aggregations (#39)
    - Extend aggregate stage to take multiple operators
    - Migrate aggregation tests

--- src/plan/aggregate.rs
+++ src/plan/aggregate.rs
@@ -6,6 +6,7 @@ use timely::worker::Worker;
 
 use differential_dataflow::difference::DiffPair;
 use differential_dataflow::operators::{Consolidate, Count, Group, Threshold};
+use differential_dataflow::operators::Join as JoinMap;
 
 use plan::Implementable;
 use Relation;
@@ -35,8 +36,9 @@ pub enum AggregationFn {
 }
 
 /// [WIP]
-/// A plan stage applying the specified aggregation function to
-/// bindings for the specified symbols. Very WIP.
+/// A plan stage applying the specified aggregation functions to
+/// bindings for the specified symbols.
+/// Given multiple aggregations we iterate and n-1 joins are applied to the results.
 #[derive(Deserialize, Clone, Debug)]
 pub struct Aggregate<P: Implementable> {
     /// TODO
@@ -44,9 +46,11 @@ pub struct Aggregate<P: Implementable> {
     /// Plan for the data source.
     pub plan: Box<P>,
     /// Logical predicate to apply.
-    pub aggregation_fn: AggregationFn,
+    pub aggregation_fns: Vec<AggregationFn>,
     /// Relation symbols that determine the grouping.
     pub key_symbols: Vec<Var>,
+    /// Aggregation symbols
+    pub aggregation_symbols: Vec<Var>,
 }
 
 impl<P: Implementable> Implementable for Aggregate<P> {
@@ -60,154 +64,202 @@ impl<P: Implementable> Implementable for Aggregate<P> {
             .implement(nested, local_arrangements, global_arrangements);
         let tuples = relation.tuples_by_symbols(&self.key_symbols);
 
-        let prepare_unary = |(key, tuple): (Vec<Value>, Vec<Value>)| {
-            let v = match tuple[0] {
-                Value::Number(num) => num,
-                _ => panic!("Can only be applied on type Number."),
-            };
-            (key.clone(), v)
-        };
+        // value_offsets are positions where inside the value vector the value for the given aggregation is found
+        let mut value_offsets = Vec::new();
+        let mut seen = Vec::new();
 
-        match self.aggregation_fn {
-            AggregationFn::MIN => SimpleRelation {
+        for sym in self.aggregation_symbols.iter() {
+            if !seen.contains(&sym) {
+                seen.push(&sym);
+                value_offsets.push(seen.iter().position(|&v| sym == v).unwrap());
+            } else {
+                value_offsets.push(seen.iter().position(|&v| sym == v).unwrap());
+            }
+        }
+
+        // output_positions keeps track of where the aggregations
+        // are supposed to be inserted to match the users specified symbol order
+        let mut variables = self.variables.clone();
+        let mut output_positions = Vec::new();
+        for sym in self.aggregation_symbols.iter() {
+            let output_index = variables.iter().position(|&v| *sym == v).unwrap();
+            output_positions.push(output_index);
+
+            variables[output_index] = 0;
+        }
+
+        let mut collections = Vec::new();
+
+        // we iterate of all aggregations and push the resulting collection 
+        for (i, aggregation_fn) in self.aggregation_fns.iter().enumerate() {
+            let value_offset = value_offsets[i];
+
+            match aggregation_fn {
+                AggregationFn::MIN =>  {
+                    let tuples = tuples
+                        .map(move |(key, tuple)| {
+                            let v = match tuple[value_offset] {
+                                Value::Number(num) => num,
+                                _ => panic!("MIN can only be applied on type Number."),
+                            };
+                            (key, v)
+                        })
+                        .group(|_key, vals, output| {
+                            let min = vals[0].0;
+                            output.push((*min, 1));
+                        })
+                        .map(move |(key, min)| {
+                            (key, vec![Value::Number(min as i64)])
+                        });
+                    collections.push(tuples);
+                },
+                AggregationFn::MAX => {
+                    let tuples = tuples
+                        .map(move |(key, tuple)| {
+                            let v = match tuple[value_offset] {
+                                Value::Number(num) => num,
+                                _ => panic!("MAX can only be applied on type Number."),
+                            };
+                            (key, v)
+                        })
+                        .group(|_key, vals, output| {
+                            let max = vals[vals.len() - 1].0;
+                            output.push((*max, 1));
+                        })
+                        .map(move |(key, max)| {
+                            (key, vec![Value::Number(max as i64)])
+                        });
+                    collections.push(tuples);
+                },
+                AggregationFn::MEDIAN =>  {
+                    let tuples = tuples
+                        .map(move |(key, tuple)| {
+                            let v = match tuple[value_offset] {
+                                Value::Number(num) => num,
+                                _ => panic!("MEDIAN can only be applied on type Number."),
+                            };
+                            (key, v)
+                        })
+                        .group(|_key, vals, output| {
+                            let median = vals[vals.len() / 2].0;
+                            output.push((*median, 1));
+                        })
+                        .map(move |(key, med)| {
+                            (key, vec![Value::Number(med as i64)])
+                        });
+                    collections.push(tuples);
+                },
+                AggregationFn::COUNT =>  {
+                    let tuples = tuples
+                        .map(move |(key, tuple)| {
+                            let v = tuple[value_offset].clone() ;
+                            (key, v)
+                        })
+                        .group(|_key, input, output| output.push((input.len(), 1)))
+                        .map(move |(key, count)| {
+                            (key, vec![Value::Number(count as i64)])
+                        });
+                    collections.push(tuples);
+                },
+                AggregationFn::SUM =>  {
+                    let tuples = tuples
+                        .map(move |(key, tuple)| {
+                            let v = match tuple[value_offset] {
+                                Value::Number(num) => num,
+                                _ => panic!("MEDIAN can only be applied on type Number."),
+                            };
+                            (key, v)
+                        })
+                        .consolidate()
+                        .distinct()
+                        .explode(|(key, val)| {
+                            Some((key, val as isize))
+                        })
+                        .count()
+                        .map(move |(key, count)| {
+                            (key, vec![Value::Number(count as i64)])
+                        });
+                    collections.push(tuples);
+                },
+                AggregationFn::AVG =>  {
+                    let tuples = tuples
+                        .map(move |(key, tuple)| {
+                            let v = match tuple[value_offset] {
+                                Value::Number(num) => num,
+                                _ => panic!("MEDIAN can only be applied on type Number."),
+                            };
+                            (key, v)
+                        })
+                        .consolidate()
+                        .distinct()
+                        .explode(move |(key, val)| {
+                            Some((key, DiffPair::new(val as isize, 1)))
+                        })
+                        .count()
+                        .map(move |(key, diff_pair)| {
+                            (key, vec![Value::Rational32(Ratio::new(
+                                diff_pair.element1 as i32,
+                                diff_pair.element2 as i32))])
+                        });
+                    collections.push(tuples);
+                },
+                AggregationFn::VARIANCE => {
+                    let tuples = tuples
+                        .map(move |(key, tuple)| {
+                            let v = match tuple[value_offset] {
+                                Value::Number(num) => num,
+                                _ => panic!("MEDIAN can only be applied on type Number."),
+                            };
+                            (key, v)
+                        })
+                        .consolidate()
+                        .distinct()
+                        .explode(move |(key, val)| {
+                            Some((key, DiffPair::new(DiffPair::new(val as isize * val as isize, val as isize), 1)))
+                        })
+                        .count()
+                        .map(move |(key, diff_pair)| {
+                            let sum_square = diff_pair.element1.element1 as i32;
+                            let sum = diff_pair.element1.element2 as i32;
+                            let c = diff_pair.element2 as i32;
+                            (key, vec![Value::Rational32(
+                                Rational32::new(sum_square, c) - Rational32::new(sum, c).pow(2))])
+                        });
+                    collections.push(tuples);
+                },
+            };
+        }
+
+        if collections.len() == 1 {
+            let output_position = output_positions[0];
+            SimpleRelation{
                 symbols: self.variables.to_vec(),
-                tuples: tuples
-                    .map(prepare_unary)
-                    .group(|_key, vals, output| {
-                        let min = vals[0].0;
-                        output.push((*min, 1));
+                tuples: collections[0]
+                    .map(move |(key, val)| {
+                        let mut k = key.clone();
+                        let v = val[0].clone();
+                        k.insert(output_position, v);
+                        k
                     })
-                    .map(|(key, min)| {
-                        let mut v = key.clone();
-                        v.push(Value::Number(min as i64));
-                        v
-                    }),
-            },
-            AggregationFn::MAX => SimpleRelation {
+            }
+        }
+        else {
+            let left = collections.remove(0);
+            let tuples = collections.iter().fold(left, |coll, next| coll.join_map(&next, |key, v1, v2| {
+                let mut val = v1.clone();
+                val.append(&mut v2.clone());
+                (key.clone(), val)
+            }));
+            SimpleRelation {
                 symbols: self.variables.to_vec(),
-                tuples: tuples
-                    .map(prepare_unary)
-                    .group(|_key, vals, output| {
-                        let max = vals[vals.len() - 1].0;
-                        output.push((*max, 1));
-                    })
-                    .map(|(key, max)| {
-                        let mut v = key.clone();
-                        v.push(Value::Number(max as i64));
-                        v
-                    }),
-            },
-            AggregationFn::MEDIAN => SimpleRelation {
-                symbols: self.variables.to_vec(),
-                tuples: tuples
-                    .map(prepare_unary)
-                    .group(|_key, vals, output| {
-                        let median = vals[vals.len() / 2].0;
-                        output.push((*median, 1));
-                    })
-                    .map(|(key, med)| {
-                        let mut v = key.clone();
-                        v.push(Value::Number(med as i64));
-                        v
-                    }),
-            },
-            AggregationFn::COUNT => SimpleRelation {
-                symbols: self.variables.to_vec(),
-                tuples: tuples
-                    .group(|_key, input, output| output.push((input.len(), 1)))
-                    .map(|(key, count)| {
-                        let mut v = key.clone();
-                        v.push(Value::Number(count as i64));
-                        v
-                    }),
-            },
-            AggregationFn::SUM => SimpleRelation {
-                symbols: self.variables.to_vec(),
-                tuples: tuples
-                    .consolidate()
-                    .distinct()
-                    .explode(|(key, tuple)| {
-                        let v = match tuple[0] {
-                            Value::Number(num) => num as isize,
-                            _ => panic!("SUM can only be applied to numbers"),
-                        };
-                        Some((key, v))
-                    })
-                    .count()
-                    .map(|(key, count)| {
-                        let mut v = key.clone();
-                        v.push(Value::Number(count as i64));
-                        v
-                    }),
-            },
-            AggregationFn::AVG => SimpleRelation {
-                symbols: self.variables.to_vec(),
-                tuples: tuples
-                    .consolidate()
-                    .distinct()
-                    .explode(|(key, tuple)| {
-                        let v = match tuple[0] {
-                            Value::Number(num) => num as isize,
-                            _ => panic!("AVG can only be applied to numbers"),
-                        };
-                        Some((key, DiffPair::new(v, 1)))
-                    })
-                    .count()
-                    .map(|(key, diff_pair)| {
-                        let mut v = key.clone();
-                        v.push(Value::Rational32(Ratio::new(
-                            diff_pair.element1 as i32,
-                            diff_pair.element2 as i32,
-                        )));
-                        v
-                    }),
-            },
-            AggregationFn::VARIANCE => SimpleRelation {
-                symbols: self.variables.to_vec(),
-                tuples: tuples
-                    .consolidate()
-                    .distinct()
-                    .explode(|(key, tuple)| {
-                        let v = match tuple[0] {
-                            Value::Number(num) => num as isize,
-                            _ => panic!("VAR can only be applied to numbers"),
-                        };
-                        Some((key, DiffPair::new(DiffPair::new(v * v, v), 1)))
-                    })
-                    .count()
-                    .map(|(key, diff_pair)| {
-                        let mut v = key.clone();
-                        let sum_square = diff_pair.element1.element1 as i32;
-                        let sum = diff_pair.element1.element2 as i32;
-                        let c = diff_pair.element2 as i32;
-                        v.push(Value::Rational32(
-                            Rational32::new(sum_square, c) - Rational32::new(sum, c).pow(2),
-                        ));
-                        v
-                    }),
-            },
-            // AggregationFn::STDDEV => SimpleRelation {
-            //     symbols: self.variables.to_vec(),
-            //     tuples: tuples
-            //         .consolidate()
-            //         .distinct()
-            //         .explode(|(key, tuple)| {
-            //             let v = match tuple[0] {
-            //                 Value::Number(num) => num as isize,
-            //                 _ => panic!("STDDEV can only be applied to numbers"),
-            //             };
-            //             Some((key, DiffPair::new(DiffPair::new(v*v, v), 1)))})
-            //         .count()
-            //         .map(|(key, diff_pair)| {
-            //             let mut v = key.clone();
-            //             let sum_square = diff_pair.element1.element1 as f64;
-            //             let sum = diff_pair.element1.element2 as f64;
-            //             let c = diff_pair.element2 as f64;
-            //
-            //             v.push(Value::Rational32(Rational32::from_float((sum_square/c - (sum/c).powi(2)).sqrt()).unwrap()));
-            //             v
-            //         }),
-            // },
+                tuples: tuples.map(move |(key, vals)|{
+                    let mut v = key.clone();
+                    for (i, val) in vals.iter().enumerate(){
+                        v.insert(output_positions[i], val.clone())
+                    }
+                    v
+                })
+            }
         }
     }
 }
