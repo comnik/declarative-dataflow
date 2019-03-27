f93392b995f94c2293f6c880b705f989721e8eb3
niko <me@nikolasgoebel.com>
2019-03-27 11:56:16 +0100

    [WIP] Bind server socket to 0.0.0.0

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -144,7 +144,8 @@ fn main() {
         let (send_errors, recv_errors) = mio::channel::channel::<(Vec<Token>, Vec<(Error, TxId)>)>();
 
         // setup server socket
-        let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), config.port);
+        // let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), config.port);
+        let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0,0,0,0)), config.port);
         let server_socket = TcpListener::bind(&addr).unwrap();
         let mut connections = Slab::with_capacity(ws_settings.max_connections);
         let mut next_connection_id: u32 = 0;
