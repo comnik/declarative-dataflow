2f1cc6edcb7e83385946a5cb6fbaacf48ad51d05
niko <me@nikolasgoebel.com>
2019-04-22 14:29:17 +0200

    Make hector_wco experiment configurarble

--- experiments/src/bin/hector_wco.rs
+++ experiments/src/bin/hector_wco.rs
@@ -1,3 +1,6 @@
+#[global_allocator]
+static ALLOCATOR: jemallocator::Jemalloc = jemallocator::Jemalloc;
+
 use graph_map::GraphMMap;
 
 use declarative_dataflow::server::Server;
@@ -8,74 +11,81 @@ use Value::Eid;
 fn main() {
     let filename = std::env::args().nth(1).unwrap();
     let batching = std::env::args().nth(2).unwrap().parse::<usize>().unwrap();
+    let query = std::env::args().nth(3).unwrap().parse::<usize>().unwrap();
     let inspect = std::env::args().any(|x| x == "inspect");
 
-    timely::execute_from_args(std::env::args().skip(2), move |worker| {
+    timely::execute_from_args(std::env::args().skip(3), move |worker| {
         let mut timer = std::time::Instant::now();
         let graph = GraphMMap::new(&filename);
         let mut server = Server::<u64, u64>::new(Default::default());
 
         // [?a :edge ?b] [?b :edge ?c] [?a :edge ?c]
         let (a, b, c) = (1, 2, 3);
-        // let plan = q(
-        //     vec![a, b, c],
-        //     vec![
-        //         Binding::attribute(a, "edge", b),
-        //         Binding::attribute(b, "edge", c),
-        //         Binding::attribute(a, "edge", c),
-        //     ],
-        // );
-
-        let plan = q(
-            vec![a, b, c],
-            vec![
-                Binding::attribute(a, "edge", b),
-                Binding::attribute(a, "edge", c),
-                Binding::attribute(b, "edge", c),
-            ],
-        );
-
-        // let plan = q(
-        //     vec![a, b, c],
-        //     vec![
-        //         Binding::attribute(b, "edge", c),
-        //         Binding::attribute(a, "edge", c),
-        //         Binding::attribute(a, "edge", b),
-        //     ],
-        // );
-
-        // ([?a ?b] [?b ?c]) [?a ?c]
-        // let plan = Plan::Join(Join {
-        //     variables: vec![a],
-        //     left_plan: Box::new(Plan::MatchA(a, "edge".to_string(), c)),
-        //     right_plan: Box::new(Plan::Join(Join {
-        //         variables: vec![b],
-        //         left_plan: Box::new(Plan::MatchA(a, "edge".to_string(), b)),
-        //         right_plan: Box::new(Plan::MatchA(b, "edge".to_string(), c)),
-        //     }))
-        // });
-
-        // ([?a ?b] [?a ?c]) [?b ?c]
-        // let plan = Plan::Join(Join {
-        //     variables: vec![b, c],
-        //     left_plan: Box::new(Plan::MatchA(b, "edge".to_string(), c)),
-        //     right_plan: Box::new(Plan::Join(Join {
-        //         variables: vec![a],
-        //         left_plan: Box::new(Plan::MatchA(a, "edge".to_string(), b)),
-        //         right_plan: Box::new(Plan::MatchA(a, "edge".to_string(), c)),
-        //     }))
-        // });
-
-        // ([?a ?c] [?b ?c]) [?a ?b]
-        // let plan = Plan::Join(Join {
-        //     variables: vec![a, b],
-        //     left_plan: Box::new(Plan::MatchA(a, "edge".to_string(), b)),
-        //     right_plan: Box::new(Plan::Join(Join {
-        //         variables: vec![c],
-        //         left_plan: Box::new(Plan::MatchA(a, "edge".to_string(), c)),
-        //         right_plan: Box::new(Plan::MatchA(b, "edge".to_string(), c)),
-        //     }))
-        // });
+        let plan =
+            if query == 0 {
+                println!("hector 1");
+                q(
+                    vec![a, b, c],
+                    vec![
+                        Binding::attribute(a, "edge", b),
+                        Binding::attribute(b, "edge", c),
+                        Binding::attribute(a, "edge", c),
+                    ],
+                )
+            } else if query == 1 {
+                println!("hector 2");
+                q(
+                    vec![a, b, c],
+                    vec![
+                        Binding::attribute(a, "edge", b),
+                        Binding::attribute(a, "edge", c),
+                        Binding::attribute(b, "edge", c),
+                    ],
+                )
+            } else if query == 2 {
+                println!("hector 3");
+                q(
+                    vec![a, b, c],
+                    vec![
+                        Binding::attribute(b, "edge", c),
+                        Binding::attribute(a, "edge", c),
+                        Binding::attribute(a, "edge", b),
+                    ],
+                )
+            } else if query == 3 {
+                println!("([?a ?b] [?b ?c]) [?a ?c]");
+                Plan::Join(Join {
+                    variables: vec![a,c],
+                    left_plan: Box::new(Plan::MatchA(a, "edge".to_string(), c)),
+                    right_plan: Box::new(Plan::Join(Join {
+                        variables: vec![b],
+                        left_plan: Box::new(Plan::MatchA(a, "edge".to_string(), b)),
+                        right_plan: Box::new(Plan::MatchA(b, "edge".to_string(), c)),
+                    }))
+                })
+            } else if query == 4 {
+                println!("([?a ?b] [?a ?c]) [?b ?c]");
+                Plan::Join(Join {
+                    variables: vec![b, c],
+                    left_plan: Box::new(Plan::MatchA(b, "edge".to_string(), c)),
+                    right_plan: Box::new(Plan::Join(Join {
+                        variables: vec![a],
+                        left_plan: Box::new(Plan::MatchA(a, "edge".to_string(), b)),
+                        right_plan: Box::new(Plan::MatchA(a, "edge".to_string(), c)),
+                    }))
+                })
+            } else {
+                println!("([?a ?c] [?b ?c]) [?a ?b]");
+                Plan::Join(Join {
+                    variables: vec![a, b],
+                    left_plan: Box::new(Plan::MatchA(a, "edge".to_string(), b)),
+                    right_plan: Box::new(Plan::Join(Join {
+                        variables: vec![c],
+                        left_plan: Box::new(Plan::MatchA(a, "edge".to_string(), c)),
+                        right_plan: Box::new(Plan::MatchA(b, "edge".to_string(), c)),
+                    }))
+                })
+            };
 
         let peers = worker.peers();
         let index = worker.index();
