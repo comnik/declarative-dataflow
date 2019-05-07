1f54b2cdefade326ac9129480eb44e32bfe6f7d9
niko <me@nikolasgoebel.com>
2019-05-07 10:17:16 +0200

    Fix cardinality_one consolidation on partially-ordered inputs

--- src/operators/mod.rs
+++ src/operators/mod.rs
@@ -1,15 +1,17 @@
 //! Extension traits for `Stream` implementing various
 //! declarative-specific operators.
 
+use timely::dataflow::channels::pact::Pipeline;
 use timely::dataflow::operators::aggregation::StateMachine;
-use timely::dataflow::operators::Map;
+use timely::dataflow::operators::{generic::operator::Operator, Map};
 use timely::dataflow::Scope;
 
 use differential_dataflow::lattice::Lattice;
-use differential_dataflow::operators::Consolidate;
+use differential_dataflow::operators::arrange::{Arrange, Arranged};
+use differential_dataflow::trace::{cursor::Cursor, BatchReader};
 use differential_dataflow::{AsCollection, Collection};
 
-use crate::Value;
+use crate::{TraceValHandle, Value};
 
 /// Provides the `cardinality_one` method.
 pub trait CardinalityOne<S: Scope> {
@@ -26,9 +28,45 @@ where
     S::Timestamp: Lattice + Ord,
 {
     fn cardinality_one(&self) -> Collection<S, (Value, Value), isize> {
-        self.consolidate()
-            .inner
-            .map(|((e, next_v), t, diff)| (e, (next_v, t, diff)))
+        let arranged: Arranged<S, TraceValHandle<Value, Value, S::Timestamp, isize>> =
+            self.arrange();
+
+        arranged
+            .stream
+            .unary(Pipeline, "AsCollection", move |_, _| {
+                move |input, output| {
+                    input.for_each(|time, data| {
+                        let mut session = output.session(&time);
+                        for wrapper in data.iter() {
+                            let batch = &wrapper;
+                            let mut cursor = batch.cursor();
+                            while let Some(key) = cursor.get_key(batch) {
+                                let mut tuples = Vec::new();
+                                while let Some(val) = cursor.get_val(batch) {
+                                    cursor.map_times(batch, |time, diff| {
+                                        tuples.push((
+                                            (key.clone(), val.clone()),
+                                            time.clone(),
+                                            diff.clone(),
+                                        ));
+                                    });
+                                    cursor.step_val(batch);
+                                }
+
+                                tuples.sort_by_key(|(_, ref t, _)| t.clone());
+                                session.give_iterator(tuples.drain(..));
+
+                                cursor.step_key(batch);
+                            }
+                        }
+                    });
+                }
+            })
+            .map(
+                |((e, next_v), t, diff): ((Value, Value), S::Timestamp, isize)| {
+                    (e, (next_v, t, diff))
+                },
+            )
             .state_machine(
                 |e, (next_v, t, diff), v| {
                     match v {
@@ -64,7 +102,7 @@ where
                     Value::Uuid(uuid) => {
                         use differential_dataflow::hashable::Hashable;
                         uuid.hashed()
-                    },
+                    }
                     _ => panic!("Not an eid or uuid: {:?}.", e),
                 },
             )
--- tests/cardinality_one.rs
+++ tests/cardinality_one.rs
@@ -1,4 +1,5 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
+use std::default::Default;
 use std::iter::FromIterator;
 use std::sync::mpsc::channel;
 use std::time::Duration;
@@ -7,103 +8,202 @@ use timely::dataflow::channels::pact::Pipeline;
 use timely::dataflow::operators::Operator;
 
 use declarative_dataflow::server::Server;
+use declarative_dataflow::timestamp::pair::Pair;
 use declarative_dataflow::timestamp::Time;
 use declarative_dataflow::{AttributeConfig, InputSemantics, Plan, Rule, TxData, Value};
 use Time::TxId;
 use Value::{Eid, Number};
 
-struct Case {
+struct Case<T> {
     description: &'static str,
     plan: Plan,
     transactions: Vec<Vec<TxData>>,
-    expectations: Vec<Vec<(Vec<Value>, u64, isize)>>,
+    expectations: Vec<Vec<(Vec<Value>, T, isize)>>,
 }
 
-fn run_cases_monotemp(mut cases: Vec<Case>) {
-    for mut case in cases.drain(..) {
-        timely::execute_directly(move |worker| {
-            let mut server = Server::<u64, u64>::new(Default::default());
-            let (send_results, results) = channel();
+trait Run {
+    fn run(self);
+}
 
-            dbg!(case.description);
+impl Run for Vec<Case<u64>> {
+    fn run(mut self) {
+        for mut case in self.drain(..) {
+            timely::execute_directly(move |worker| {
+                let mut server = Server::<u64, u64>::new(Default::default());
+                let (send_results, results) = channel();
 
-            let plan = case.plan;
+                dbg!(case.description);
 
-            let mut deps = HashSet::new();
-            for tx in case.transactions.iter() {
-                for datum in tx {
-                    deps.insert(datum.2.clone());
-                }
-            }
-
-            worker.dataflow::<u64, _, _>(|scope| {
-                for dep in deps.iter() {
-                    let config = AttributeConfig::tx_time(InputSemantics::CardinalityOne);
-                    server
-                        .context
-                        .internal
-                        .create_transactable_attribute(dep, config, scope)
-                        .unwrap();
-                }
-
-                server
-                    .test_single(
-                        scope,
-                        Rule {
-                            name: "query".to_string(),
-                            plan,
-                        },
-                    )
-                    .inner
-                    .sink(Pipeline, "Results", move |input| {
-                        input.for_each(|_time, data| {
-                            for datum in data.iter() {
-                                send_results.send(datum.clone()).unwrap()
-                            }
+                let mut deps = HashMap::new();
+                for tx in case.transactions.iter() {
+                    for datum in tx {
+                        deps.entry(datum.2.clone()).or_insert_with(|| {
+                            AttributeConfig::tx_time(InputSemantics::CardinalityOne)
                         });
-                    });
-            });
-
-            let mut next_tx = 0;
-
-            // Make sure we can pop off transactions one-by-one.
-            case.transactions.reverse();
-
-            for mut expected_tuples in case.expectations.drain(..) {
-                next_tx += 1;
-
-                if let Some(tx_data) = case.transactions.pop() {
-                    server.transact(tx_data, 0, 0).unwrap();
-                }
-
-                server.advance_domain(None, next_tx).unwrap();
-
-                worker.step_while(|| server.is_any_outdated());
-
-                let mut expected: HashSet<(Vec<Value>, u64, isize)> =
-                    HashSet::from_iter(expected_tuples.drain(..));
-
-                for _i in 0..expected.len() {
-                    let result = results
-                        .recv_timeout(Duration::from_millis(400))
-                        .expect("no result");
-
-                    if !expected.remove(&result) {
-                        panic!("Unknown result {:?}.", result);
                     }
                 }
 
-                if let Ok(result) = results.recv_timeout(Duration::from_millis(400)) {
-                    panic!("Extraneous result {:?}", result);
+                let plan = case.plan;
+
+                worker.dataflow::<u64, _, _>(|scope| {
+                    for (dep, config) in deps.drain() {
+                        server
+                            .context
+                            .internal
+                            .create_transactable_attribute(&dep, config, scope)
+                            .unwrap();
+                    }
+
+                    server
+                        .test_single(
+                            scope,
+                            Rule {
+                                name: "query".to_string(),
+                                plan,
+                            },
+                        )
+                        .inner
+                        .sink(Pipeline, "Results", move |input| {
+                            input.for_each(|_time, data| {
+                                for datum in data.iter() {
+                                    send_results.send(datum.clone()).unwrap()
+                                }
+                            });
+                        });
+                });
+
+                let mut next_tx = 0;
+
+                // Make sure we can pop off transactions one-by-one.
+                case.transactions.reverse();
+
+                for mut expected_tuples in case.expectations.drain(..) {
+                    next_tx += 1;
+
+                    if let Some(tx_data) = case.transactions.pop() {
+                        server.transact(tx_data, 0, 0).unwrap();
+                    }
+
+                    server.advance_domain(None, next_tx).unwrap();
+
+                    worker.step_while(|| server.is_any_outdated());
+
+                    let mut expected: HashSet<(Vec<Value>, u64, isize)> =
+                        HashSet::from_iter(expected_tuples.drain(..));
+
+                    for _i in 0..expected.len() {
+                        let result = results
+                            .recv_timeout(Duration::from_millis(400))
+                            .expect("no result");
+
+                        if !expected.remove(&result) {
+                            panic!("Unknown result {:?}.", result);
+                        }
+                    }
+
+                    if let Ok(result) = results.recv_timeout(Duration::from_millis(400)) {
+                        panic!("Extraneous result {:?}", result);
+                    }
                 }
-            }
-        });
+            });
+        }
+    }
+}
+
+impl Run for Vec<Case<Pair<Duration, u64>>> {
+    fn run(mut self) {
+        for mut case in self.drain(..) {
+            timely::execute_directly(move |worker| {
+                let mut server = Server::<Pair<Duration, u64>, u64>::new(Default::default());
+                let (send_results, results) = channel();
+
+                dbg!(case.description);
+
+                let mut deps = HashMap::new();
+                for tx in case.transactions.iter() {
+                    for datum in tx {
+                        deps.entry(datum.2.clone())
+                            .or_insert_with(|| AttributeConfig {
+                                input_semantics: InputSemantics::CardinalityOne,
+                                trace_slack: Some(Time::Bi(Duration::from_secs(0), 1)),
+                                ..Default::default()
+                            });
+                    }
+                }
+
+                let plan = case.plan;
+
+                worker.dataflow::<Pair<Duration, u64>, _, _>(|scope| {
+                    for (dep, config) in deps.drain() {
+                        server
+                            .context
+                            .internal
+                            .create_transactable_attribute(&dep, config, scope)
+                            .unwrap();
+                    }
+
+                    server
+                        .test_single(
+                            scope,
+                            Rule {
+                                name: "query".to_string(),
+                                plan,
+                            },
+                        )
+                        .inspect(|x| println!("{:?}", x))
+                        .inner
+                        .sink(Pipeline, "Results", move |input| {
+                            input.for_each(|_time, data| {
+                                for datum in data.iter() {
+                                    send_results.send(datum.clone()).unwrap()
+                                }
+                            });
+                        });
+                });
+
+                let mut next_tx = 0;
+
+                // Make sure we can pop off transactions one-by-one.
+                case.transactions.reverse();
+
+                for mut expected_tuples in case.expectations.drain(..) {
+                    next_tx += 1;
+
+                    if let Some(tx_data) = case.transactions.pop() {
+                        server.transact(tx_data, 0, 0).unwrap();
+                    }
+
+                    server
+                        .advance_domain(None, Pair::new(Duration::from_secs(next_tx), next_tx))
+                        .unwrap();
+
+                    worker.step_while(|| server.is_any_outdated());
+
+                    let mut expected: HashSet<(Vec<Value>, Pair<Duration, u64>, isize)> =
+                        HashSet::from_iter(expected_tuples.drain(..));
+
+                    for _i in 0..expected.len() {
+                        let result = results
+                            .recv_timeout(Duration::from_millis(400))
+                            .expect("no result");
+
+                        if !expected.remove(&result) {
+                            panic!("Unknown result {:?}.", result);
+                        }
+                    }
+
+                    if let Ok(result) = results.recv_timeout(Duration::from_millis(400)) {
+                        panic!("Extraneous result {:?}", result);
+                    }
+                }
+            });
+        }
     }
 }
 
 #[test]
 fn cardinality_one() {
-    run_cases_monotemp(vec![
+    vec![
         Case {
             description: "happy case",
             plan: Plan::MatchA(0, ":amount".to_string(), 1),
@@ -164,12 +264,33 @@ fn cardinality_one() {
                 vec![(vec![Eid(200), Number(100)], 1, -1)],
             ],
         },
-    ]);
+        Case {
+            description: "toggle",
+            plan: Plan::MatchA(0, ":amount".to_string(), 1),
+            transactions: vec![
+                vec![TxData::add(100, ":amount", Number(5))],
+                vec![TxData::add(100, ":amount", Number(10))],
+                vec![TxData::add(100, ":amount", Number(5))],
+            ],
+            expectations: vec![
+                vec![(vec![Eid(100), Number(5)], 0, 1)],
+                vec![
+                    (vec![Eid(100), Number(5)], 1, -1),
+                    (vec![Eid(100), Number(10)], 1, 1),
+                ],
+                vec![
+                    (vec![Eid(100), Number(10)], 2, -1),
+                    (vec![Eid(100), Number(5)], 2, 1),
+                ],
+            ],
+        },
+    ]
+    .run();
 }
 
 #[test]
 fn cardinality_one_unordered() {
-    run_cases_monotemp(vec![Case {
+    vec![Case {
         description: "late arrival",
         plan: Plan::MatchA(0, ":amount".to_string(), 1),
         transactions: vec![
@@ -190,5 +311,80 @@ fn cardinality_one_unordered() {
                 (vec![Eid(100), Number(2)], 2, 1),
             ],
         ],
-    }]);
+    }]
+    .run();
+}
+
+#[test]
+fn bitemporal_toggle() {
+    vec![Case {
+        description: "bitemporal toggle",
+        plan: Plan::MatchA(0, ":flow".to_string(), 1),
+        transactions: vec![vec![
+            TxData(
+                1,
+                Value::uuid_str("71828aae-4fc8-421b-82ca-68c5f4981d74"),
+                ":flow".to_string(),
+                Value::from(30.006),
+                Some(Time::Bi(Duration::from_secs(0), 1554120030000)), // 2019-04-01T12:00:30+00:00
+            ),
+            TxData(
+                1,
+                Value::uuid_str("71828aae-4fc8-421b-82ca-68c5f4981d74"),
+                ":flow".to_string(),
+                Value::from(31.006),
+                Some(Time::Bi(Duration::from_secs(0), 1554120061000)), // 2019-04-01T12:01:01+00:00
+            ),
+            TxData(
+                1,
+                Value::uuid_str("71828aae-4fc8-421b-82ca-68c5f4981d74"),
+                ":flow".to_string(),
+                Value::from(30.006),
+                Some(Time::Bi(Duration::from_secs(0), 1554120150000)), // 2019-04-01T12:02:30+00:00
+            ),
+        ]],
+        expectations: vec![vec![
+            (
+                vec![
+                    Value::uuid_str("71828aae-4fc8-421b-82ca-68c5f4981d74"),
+                    Value::from(30.006),
+                ],
+                Pair::new(Duration::from_secs(0), 1554120030000),
+                1,
+            ),
+            (
+                vec![
+                    Value::uuid_str("71828aae-4fc8-421b-82ca-68c5f4981d74"),
+                    Value::from(30.006),
+                ],
+                Pair::new(Duration::from_secs(0), 1554120061000),
+                -1,
+            ),
+            (
+                vec![
+                    Value::uuid_str("71828aae-4fc8-421b-82ca-68c5f4981d74"),
+                    Value::from(31.006),
+                ],
+                Pair::new(Duration::from_secs(0), 1554120061000),
+                1,
+            ),
+            (
+                vec![
+                    Value::uuid_str("71828aae-4fc8-421b-82ca-68c5f4981d74"),
+                    Value::from(31.006),
+                ],
+                Pair::new(Duration::from_secs(0), 1554120150000),
+                -1,
+            ),
+            (
+                vec![
+                    Value::uuid_str("71828aae-4fc8-421b-82ca-68c5f4981d74"),
+                    Value::from(30.006),
+                ],
+                Pair::new(Duration::from_secs(0), 1554120150000),
+                1,
+            ),
+        ]],
+    }]
+    .run();
 }
