6367dafc479e0c2c10b9a99636d9a4ac3195cc50
niko <me@nikolasgoebel.com>
2019-07-15 23:37:23 +0200

    [WIP] Cleanup pull_test

--- tests/pull_test.rs
+++ tests/pull_test.rs
@@ -6,8 +6,7 @@ use std::time::Duration;
 use timely::dataflow::channels::pact::Pipeline;
 use timely::dataflow::operators::Operator;
 
-use declarative_dataflow::binding::Binding;
-use declarative_dataflow::plan::{Implementable, Pull, PullLevel};
+use declarative_dataflow::plan::{Implementable, PullLevel};
 use declarative_dataflow::server::Server;
 use declarative_dataflow::timestamp::Time;
 use declarative_dataflow::{AttributeConfig, IndexDirection, QuerySupport};
@@ -161,171 +160,12 @@ fn pull_level() {
     }]);
 }
 
-#[test]
-fn pull_children() {
-    let (parent, child) = (1, 2);
-
-    run_cases(vec![Case {
-        description: "[:find (pull ?child [:name :age]) :where [_ :parent/child ?child]]",
-        plan: Plan::PullLevel(PullLevel {
-            variables: vec![],
-            pull_variable: child,
-            plan: Box::new(Plan::MatchA(parent, "parent/child".to_string(), child)),
-            pull_attributes: vec!["name".to_string(), "age".to_string()],
-            path_attributes: vec!["parent/child".to_string()],
-            cardinality_many: false,
-        }),
-        transactions: vec![vec![
-            TxData::add(100, "name", String("Alice".to_string())),
-            TxData::add(100, "parent/child", Eid(300)),
-            TxData::add(200, "name", String("Bob".to_string())),
-            TxData::add(200, "parent/child", Eid(400)),
-            TxData::add(300, "name", String("Mabel".to_string())),
-            TxData::add(300, "age", Number(13)),
-            TxData::add(400, "name", String("Dipper".to_string())),
-            TxData::add(400, "age", Number(12)),
-        ]],
-        expectations: vec![vec![
-            (
-                vec![
-                    Eid(100),
-                    Aid("parent/child".to_string()),
-                    Eid(300),
-                    Aid("age".to_string()),
-                    Number(13),
-                ],
-                0,
-                1,
-            ),
-            (
-                vec![
-                    Eid(100),
-                    Aid("parent/child".to_string()),
-                    Eid(300),
-                    Aid("name".to_string()),
-                    String("Mabel".to_string()),
-                ],
-                0,
-                1,
-            ),
-            (
-                vec![
-                    Eid(200),
-                    Aid("parent/child".to_string()),
-                    Eid(400),
-                    Aid("age".to_string()),
-                    Number(12),
-                ],
-                0,
-                1,
-            ),
-            (
-                vec![
-                    Eid(200),
-                    Aid("parent/child".to_string()),
-                    Eid(400),
-                    Aid("name".to_string()),
-                    String("Dipper".to_string()),
-                ],
-                0,
-                1,
-            ),
-        ]],
-    }]);
-}
-
-#[test]
-fn pull() {
-    let (a, b, c) = (1, 2, 3);
-
-    run_cases(vec![Case {
-        description:
-            "[:find (pull ?a [:name {:join/binding #:pattern[e a v]}]) :where [?a :join/binding]]",
-        plan: Plan::Pull(Pull {
-            variables: vec![],
-            paths: vec![
-                Plan::PullLevel(PullLevel {
-                    variables: vec![],
-                    pull_variable: b,
-                    plan: Box::new(Plan::MatchA(a, "join/binding".to_string(), b)),
-                    pull_attributes: vec![
-                        "pattern/e".to_string(),
-                        "pattern/a".to_string(),
-                        "pattern/v".to_string(),
-                    ],
-                    path_attributes: vec!["join/binding".to_string()],
-                    cardinality_many: false,
-                }),
-                Plan::PullLevel(PullLevel {
-                    variables: vec![],
-                    pull_variable: c,
-                    plan: Box::new(Plan::MatchA(a, "name".to_string(), c)),
-                    pull_attributes: vec![],
-                    path_attributes: vec!["name".to_string()],
-                    cardinality_many: false,
-                }),
-            ],
-        }),
-        transactions: vec![vec![
-            TxData::add(100, "name", String("rule".to_string())),
-            TxData::add(100, "join/binding", Eid(200)),
-            TxData::add(100, "join/binding", Eid(300)),
-            TxData::add(200, "pattern/a", Aid("xyz".to_string())),
-            TxData::add(300, "pattern/e", Eid(12345)),
-            TxData::add(300, "pattern/a", Aid("asd".to_string())),
-        ]],
-        expectations: vec![vec![
-            (
-                vec![
-                    Eid(100),
-                    Aid("name".to_string()),
-                    String("rule".to_string()),
-                ],
-                0,
-                1,
-            ),
-            (
-                vec![
-                    Eid(100),
-                    Aid("join/binding".to_string()),
-                    Eid(200),
-                    Aid("pattern/a".to_string()),
-                    Aid("xyz".to_string()),
-                ],
-                0,
-                1,
-            ),
-            (
-                vec![
-                    Eid(100),
-                    Aid("join/binding".to_string()),
-                    Eid(300),
-                    Aid("pattern/e".to_string()),
-                    Eid(12345),
-                ],
-                0,
-                1,
-            ),
-            (
-                vec![
-                    Eid(100),
-                    Aid("join/binding".to_string()),
-                    Eid(300),
-                    Aid("pattern/a".to_string()),
-                    Aid("asd".to_string()),
-                ],
-                0,
-                1,
-            ),
-        ]],
-    }]);
-}
-
 #[cfg(feature = "graphql")]
 #[test]
 #[rustfmt::skip]
 fn graph_ql() {
     use declarative_dataflow::plan::GraphQl;
+    use declarative_dataflow::binding::Binding;
 
     let transactions = vec![vec![        
         TxData::add(100, "name", Value::from("Alice")),
@@ -394,6 +234,8 @@ fn graph_ql() {
 
             let expectations = vec![vec![
                 (vec![Eid(300), Value::aid("bested"), Eid(400), Value::aid("age"), Number(12)], 0, 1),
+                (vec![Eid(200), Value::aid("bested"), Eid(100), Value::aid("db__id"), Eid(100)], 0, 1),
+                (vec![Eid(300), Value::aid("bested"), Eid(400), Value::aid("db__id"), Eid(400)], 0, 1),
             ]];
 
             Case {
